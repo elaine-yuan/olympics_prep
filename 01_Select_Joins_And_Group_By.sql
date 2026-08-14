8c8fb93bf1506cf1636e82d17e423edd365509dd
Elaine Yuan <107414853+elaine-yuan@users.noreply.github.com>
2026-08-14 12:07:30 -0400

    Update 01_Select_Joins_And_Group_By.sql

--- 01_Select_Joins_And_Group_By.sql
+++ 01_Select_Joins_And_Group_By.sql
@@ -8,11 +8,8 @@
 --Requirements: Exclude any Winter editions. Join from athletes through results to editions. Ensure you count distinct edition_ids, not just total gold medal rows.
 
 SELECT a.athlete_id, a.full_name, COUNT(DISTINCT e.edition_id) AS count_of_summer_editions
-FROM athletes a 
-LEFT JOIN results r
-ON a.athlete_id=r.athlete_id
-LEFT JOIN editions e
-ON r.edition_id=e.edition_id
+FROM athletes a LEFT JOIN results r ON a.athlete_id=r.athlete_id
+LEFT JOIN editions e ON r.edition_id=e.edition_id
 WHERE a.sex='Female' AND e.season='Summer' AND r.medal='Gold'
 GROUP BY 1,2
 HAVING COUNT(DISTINCT e.edition_id)>=2
@@ -26,11 +23,8 @@ SELECT n.country_name, e.edition_year,
 SUM(CASE WHEN season='Summer' AND medal IS NOT NULL THEN 1 ELSE 0 END) AS summer_medals, 
 SUM(CASE WHEN season='Winter' AND medal IS NOT NULL THEN 1 ELSE 0 END) AS winter_medals, 
 SUM(CASE WHEN medal IS NOT NULL THEN 1 ELSE 0 END) AS total_medals
-FROM nocs n
-LEFT JOIN results r 
-ON n.noc=r.noc_id 
-LEFT JOIN editions e
-ON r.edition_id=e.edition_id
+FROM nocs n LEFT JOIN results r ON n.noc=r.noc_id 
+LEFT JOIN editions e ON r.edition_id=e.edition_id
 GROUP BY 1,2
 HAVING summer_medals>=1 AND winter_medals>=1
 
@@ -40,18 +34,13 @@ HAVING summer_medals>=1 AND winter_medals>=1
 --Requirements: Join athletes  results  events  disciplines  sport_groups. Filter out records where height_cm or weight_kg is NULL. Filter the final aggregated group results to show only sports groups where avg_height_cm is strictly greater than 180.0 cm.
 
 SELECT s.sports_group_name, AVG(a.height_cm) AS avg_height_cm, AVG(a.weight_kg) AS avg_weight_kg, COUNT(r.medal) AS total_medalist_records
-FROM athletes a
-JOIN results r
-ON a.athlete_id = r.athlete_id
-JOIN events e
-ON r.event_id = e.event_id
-JOIN disciplines d
-ON e.discipline_id = d.discipline_id
-JOIN sport_groups s
-ON d.sports_group_id = s.sports_group_id
+FROM athletes a JOIN results r ON a.athlete_id=r.athlete_id
+JOIN events e ON r.event_id=e.event_id
+JOIN disciplines d ON e.discipline_id=d.discipline_id
+JOIN sport_groups s ON d.sports_group_id=s.sports_group_id
 WHERE r.medal IS NOT NULL AND a.height_cm IS NOT NULL AND a.weight_kg IS NOT NULL
 GROUP BY 1
-HAVING avg_height_cm > 180
+HAVING avg_height_cm>180
 
 --Q1.4: Solo vs. Team Country Performance
 --Compare solo vs. team medal performance for each country era (country_era in nocs).
@@ -59,12 +48,10 @@ HAVING avg_height_cm > 180
 --Requirements: Filter out rows where medal IS NULL. Aggregate solo medals (team_or_solo = 'solo') and team medals (team_or_solo = 'team'). Include only country eras that have won at least 20 total combined medals.
 
 SELECT country_era, SUM(CASE WHEN team_or_solo='Solo' THEN 1 ELSE 0 END) AS solo_medals, SUM(CASE WHEN team_or_solo='Team' THEN 1 ELSE 0 END) AS  team_medals, COUNT(medal) AS total_medals
-FROM nocs n
-JOIN results r
-ON n.noc=r.noc_id
+FROM nocs n JOIN results r ON n.noc=r.noc_id
 WHERE medal IS NOT NULL
 GROUP BY 1
-HAVING total_medals >=20
+HAVING total_medals>=20
 
 --Q1.5: Multi-Discipline Medalists in a Single Edition
 --Find all athletes who won medals in more than one distinct discipline during the same Olympic edition.
@@ -72,15 +59,10 @@ HAVING total_medals >=20
 --Requirements: Join athletes, results, events, disciplines, and editions. Group by athlete and edition, and use HAVING COUNT(DISTINCT discipline_id) > 1.
 
 SELECT a.athlete_id, a.full_name, ed.edition_id, ed.edition_year, COUNT(DISTINCT d.discipline_id) AS distinct_disciplines_count
-FROM athletes a
-JOIN results r
-ON a.athlete_id=r.athlete_id
-JOIN editions ed
-ON r.edition_id=ed.edition_id
-JOIN events e
-ON r.event_id=e.event_id
-JOIN disciplines d
-on e.discipline_id=d.discipline_id
+FROM athletes a JOIN results r ON a.athlete_id=r.athlete_id
+JOIN editions ed ON r.edition_id=ed.edition_id
+JOIN events e ON r.event_id=e.event_id
+JOIN disciplines d on e.discipline_id=d.discipline_id
 WHERE medal IS NOT NULL
 GROUP BY 1,2,3,4
 HAVING distinct_disciplines_count>1
@@ -91,11 +73,8 @@ HAVING distinct_disciplines_count>1
 --Requirements: Join nocs, results, and events. Use conditional aggregation to break down discontinued gold medals versus total discontinued medals.
 
 SELECT n.country_name, SUM(IFF(r.medal = 'Gold', 1, 0)) AS discontinued_gold_count, SUM(IFF(r.medal IS NOT NULL, 1, 0)) AS total_discontinued_medals
-FROM nocs n
-JOIN results r
-ON n.noc = r.noc_id
-JOIN events e
-ON r.event_id = e.event_id
-WHERE e.is_event_discontinued = TRUE 
+FROM nocs n JOIN results r ON n.noc=r.noc_id
+JOIN events e ON r.event_id=e.event_id
+WHERE e.is_event_discontinued=TRUE 
 GROUP BY 1
-HAVING total_discontinued_medals >= 5
+HAVING total_discontinued_medals>=5
